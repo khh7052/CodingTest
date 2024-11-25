65eab03d4299a5818fcd33661ca65ce41ee3ff09
KimHyeHyeon <59150989+khh7052@users.noreply.github.com>
2024-11-25 12:02:57 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, vector<int> numlist) {
+    vector<int> answer;
+    
+    for(const auto i : numlist){
+        if(i % n == 0){
+            answer.push_back(i);
+        }
+    }
+    
+    return answer;
+}
