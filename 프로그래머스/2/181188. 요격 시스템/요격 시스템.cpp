9704052e11ba291be24cb163cdd3a5be3dc97d78
KimHyeHyeon <59150989+khh7052@users.noreply.github.com>
2024-11-29 18:11:03 +0900

    [level 2] Title: 요격 시스템, Time: 852.63 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <functional>
+
+using namespace std;
+
+int solution(vector<vector<int>> targets) {
+    
+    struct cmp {
+        bool operator()(vector<int> a, vector<int> b) {
+            return a[1] > b[1];
+        }
+    };
+    
+    priority_queue<vector<int>, vector<vector<int>>, cmp> q;
+    
+    for(auto& target : targets){
+        q.push(target);
+    }
+    
+    int answer = 0;
+    
+    while (!q.empty()) {
+        int end = q.top()[1];
+        
+        cout << q.top()[0] << " " << q.top()[1] << '\n';
+        q.pop();
+        answer++;
+        
+        while (!q.empty()){
+            int nextStart = q.top()[0];
+            
+            if(end > nextStart){
+                q.pop();
+            }
+            else{
+                break;
+            }
+        }
+        
+        
+    }
+    
+    
+    return answer;
+}
