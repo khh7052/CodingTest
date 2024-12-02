6d524a7380c80f10e67b40747ecd25df556d3b3c
KimHyeHyeon <59150989+khh7052@users.noreply.github.com>
2024-12-03 07:24:59 +0900

    [level 2] Title: 타겟 넘버, Time: 75.67 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -3,23 +3,23 @@
 
 using namespace std;
 
-int dfs(int i, vector<int> numbers, int end, int sum, int target);
+int dfs(int i, vector<int> numbers, int sum, int target);
 
 int solution(vector<int> numbers, int target) {
     int answer = 0;
     
-    answer = dfs(0, numbers, numbers.size(), 0, target);
+    answer = dfs(0, numbers, 0, target);
     
     return answer;
 }
 
-int dfs(int i, vector<int> numbers, int end, int sum, int target){
+int dfs(int i, vector<int> numbers, int sum, int target){
     
-    if(i >= end){
+    if(i >= numbers.size()){
         if(sum == target) return 1;
         else return 0;
     }
     else{
-        return dfs(i+1, numbers, end, sum + numbers[i], target) + dfs(i+1, numbers, end, sum - numbers[i], target);
+        return dfs(i+1, numbers, sum + numbers[i], target) + dfs(i+1, numbers, sum - numbers[i], target);
     }
 }
