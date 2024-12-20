33cf549a2349457e8b0c6933d91ccc3e3863716e
KimHyeHyeon <59150989+khh7052@users.noreply.github.com>
2024-12-20 12:07:02 +0900

    [level 2] Title: 숫자 변환하기, Time: 8.34 ms, Memory: 7.89 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -1,29 +1,25 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <queue>
-
 using namespace std;
 
-int solution(int x, int y, int n) {
+int solution(int x, int y, int n)
+{
+    vector<int> vis(1000001,0);
     queue<int> q;
-    vector<int> visited(y+1, 0);
-    
     q.push(x);
-    while(!q.empty()){
-        int num = q.front(); q.pop();
-        
-        if(num == y) return visited[num];
-        
-        int dx[3] = {num + n, num * 2, num * 3};
-        
-        for(int i = 0; i < 3; i++){
-            if(dx[i] > y || visited[dx[i]] > 0) continue;
-            visited[dx[i]] = visited[num] + 1;
-            q.push(dx[i]);
+    while (!q.empty())
+    {
+        int cur = q.front(); q.pop();
+        if(cur == y) return vis[cur];//시작하자마자 도착할수도있다.
+        int dx[3] = {cur+n, cur*2, cur*3};
+        for(int dir=0;dir<3;dir++)
+        {
+            int nx = dx[dir];
+            if(nx > y || vis[nx]!= 0) continue;//범위를 넘어가거나 이미 방문한적 있다면
+            vis[nx] = vis[cur] + 1;
+            q.push(nx);
         }
-        
     }
-    
     return -1;
 }
