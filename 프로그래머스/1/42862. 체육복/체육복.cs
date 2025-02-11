f75041de089c6d5f9d207620c68e7e8b90d3a76a
KimHyeHyeon <59150989+khh7052@users.noreply.github.com>
2025-02-11 14:41:54 +0900

    [level 1] Title: 체육복, Time: 3.01 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        Array.Sort(lost);
+        Array.Sort(reserve);
+        
+        for(int i = 0; i < lost.Length; i++){    
+            for(int j = 0; j < reserve.Length; j++){
+                if(lost[i] == reserve[j]){
+                    lost[i] = 0;
+                    reserve[j] = 0;
+                }
+            }
+        }
+        
+        Array.Sort(lost);
+        Array.Sort(reserve);
+        
+        Queue<int> lostQ = new Queue<int>(lost);
+        Queue<int> reserveQ = new Queue<int>(reserve);
+        int lostCount = 0;
+        
+        while(lostQ.Count > 0){
+            if(lostQ.Peek() != 0) break;
+            lostQ.Dequeue();
+            reserveQ.Dequeue();
+        }
+        
+        while(lostQ.Count > 0 && reserveQ.Count > 0){
+            int lostNum = lostQ.Peek();
+            int reserveNum = reserveQ.Peek();
+            //Console.WriteLine($"Lost {lostNum} Reserve {reserveNum}");
+            if(lostNum + 1 < reserveNum){
+                lostCount++;
+                lostQ.Dequeue();
+            }
+            else if(lostNum > reserveNum + 1){
+                reserveQ.Dequeue();
+            }
+            else{
+                reserveQ.Dequeue();
+                lostQ.Dequeue();
+            }
+        }
+        
+        lostCount += lostQ.Count;
+        //Console.WriteLine($"Lost {lostCount}");
+        
+        return n - lostCount;
+    }
+}
