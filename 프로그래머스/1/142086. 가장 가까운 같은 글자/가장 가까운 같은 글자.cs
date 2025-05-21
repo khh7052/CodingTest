45f902ab450329d1e1505859e955d04a9dcf3d1b
KimHyeHyeon <59150989+khh7052@users.noreply.github.com>
2025-05-21 16:22:11 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.32 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cs
@@ -0,0 +1,22 @@
+using System;
+
+
+public class Solution {
+    public int[] solution(string s) {
+        int[] answer = new int[s.Length];
+        
+        for(int i = 0; i < s.Length; i++){
+            answer[i] = -1;
+            
+            for(int j = i-1; j >= 0; j--){
+                if(s[i] == s[j]){
+                    answer[i] = i - j;
+                    break;
+                }
+            }
+        }
+        
+        
+        return answer;
+    }
+}
