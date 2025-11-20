fc45dc8b21676f38aa10d8d019ec2ac0f2611c61
KimHyeHyeon <59150989+khh7052@users.noreply.github.com>
2025-11-20 19:04:40 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.83 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cs
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cs
@@ -1,28 +1,27 @@
 using System;
+using System.Text;
 
 public class Solution {
     public int solution(string my_string) {
         int answer = 0;
-        int start = 0;
-        int length = 0;
+        StringBuilder sb = new StringBuilder();
         
         for(int i = 0; i < my_string.Length; i++){
             if(char.IsDigit(my_string[i])){
-                if(length == 0) start = i;
-                length++;
+                sb.Append(my_string[i]);
             }
             else{
-                if(length > 0){
-                    answer += int.Parse(my_string.Substring(start, length));
-                    length = 0;
+                if(sb.Length > 0){
+                    answer += int.Parse(sb.ToString());
+                    sb.Clear();
                 }
             }
         }
         
-                if(length > 0){
-                    answer += int.Parse(my_string.Substring(start, length));
-                    length = 0;
-                }
+        if(sb.Length > 0){
+            answer += int.Parse(sb.ToString());
+            sb.Clear();
+        }
         
         return answer;
     }
