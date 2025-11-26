4455dec019cd061fc9bc63ea1a6f595b9c6b843c
KimHyeHyeon <59150989+khh7052@users.noreply.github.com>
2025-11-26 11:25:46 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int chicken) {
+        int answer = 0;
+        int coupon = chicken;
+        
+        while(coupon >= 10){
+            int service = coupon / 10;
+            answer += service;
+            coupon = service + coupon % 10;
+        }
+        
+        return answer;
+    }
+}
