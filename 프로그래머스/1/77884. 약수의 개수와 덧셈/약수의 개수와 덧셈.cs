67dd081d7a88b062efa8eea2629435001dd37a8d
KimHyeHyeon <59150989+khh7052@users.noreply.github.com>
2025-11-29 00:27:03 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.29 ms, Memory: 31.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+
+        for (int i = left; i <= right; i++)
+            answer += (Math.Sqrt(i) % 1 == 0) ? -i : i;
+
+        return answer;
+    }
+}
