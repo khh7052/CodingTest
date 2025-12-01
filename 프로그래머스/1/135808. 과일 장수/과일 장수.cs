dff4530127f113af2a7850c0082f39fa4d69931a
KimHyeHyeon <59150989+khh7052@users.noreply.github.com>
2025-12-01 23:26:24 +0900

    [level 1] Title: 과일 장수, Time: 106.99 ms, Memory: 50.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Array.Sort(score, (a, b) => b.CompareTo(a));
+        
+        for(int i = m-1; i < score.Length; i+=m)
+            answer += score[i] * m;
+        
+        return answer;
+    }
+}
