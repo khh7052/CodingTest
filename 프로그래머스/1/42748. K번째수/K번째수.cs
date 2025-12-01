3ca476fb4eef98dd212d1b84027e94527040dc46
KimHyeHyeon <59150989+khh7052@users.noreply.github.com>
2025-12-01 21:59:38 +0900

    [level 1] Title: K번째수, Time: 4.01 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int[] array, int[,] commands) {
+        int n = commands.GetLength(0);
+        int[] answer = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            int skip = commands[i, 0] - 1;
+            int take = commands[i, 1] - skip;
+            int selectIndex = commands[i, 2] - 1;
+            
+            answer[i] = array
+                .Skip(skip)
+                .Take(take)
+                .OrderBy(x => x)
+                .ToArray()[selectIndex];
+        }
+        
+        return answer;
+    }
+}
